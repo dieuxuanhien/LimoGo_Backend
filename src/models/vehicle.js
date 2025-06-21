7d69b1d8fec64c5df97a266bc5b66e43a446588a
23520187 <23520187@gm.uit.edu.vn>
2025-06-21 13:39:13 +0700

    Revert "."
    This reverts commit b061abd91adf877ff67c194322a0c8836437df7c.

--- src/models/vehicle.js
+++ src/models/vehicle.js
@@ -5,7 +5,6 @@ const vehicleSchema = new mongoose.Schema({
   type: { type: String, required: true },
   currentStation: { type: mongoose.Schema.Types.ObjectId, ref: 'Station', required: true },
   licensePlate: { type: String, required: true, unique: true },
-  status: { type: String, enum: ['available', 'in-use', 'maintenance'], default: 'available' },
   capacity: { type: Number, required: true },
   manufacturer: { type: String },
   model: { type: String },
