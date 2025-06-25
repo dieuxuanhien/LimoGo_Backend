6dedd0a95e39649d2bd7592e89868f1ed8958b2b
23520187 <23520187@gm.uit.edu.vn>
2025-06-26 06:10:47 +0700

    modify station model

--- src/models/station.js
+++ src/models/station.js
@@ -1,8 +1,24 @@
 const { Schema, model } = require('mongoose');
 
 const stationSchema = new Schema({
-  name: { type: String, required: true, unique: true },
+
+  name: { type: String, required: true }, // Tên bến xe hoặc điểm đón
   city: { type: String, required: true, index: true },
+  address: { type: String, required: true }, // Thêm địa chỉ chi tiết
+
+  type: {
+        type: String,
+        enum: ['main_station', 'pickup_point'], // main_station: bến xe chính thức, pickup_point: điểm đón/trả riêng
+        required: true
+  },
+
+  // --- TRƯỜNG MỚI ĐỂ XÁC ĐỊNH CHỦ SỞ HỮU ---
+    // Chỉ có giá trị khi type là 'pickup_point'
+    ownerProvider: {
+        type: Schema.Types.ObjectId,
+        ref: 'Provider'
+    },
+
   country: { type: String, default: 'VietNam' },
   coordinates: {
     lat: { type: Number },
